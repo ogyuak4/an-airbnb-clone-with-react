742212e81604f0467f58a02ca13cb6dddd527e00
ogyuak4 <93539160+ogyuak4@users.noreply.github.com>
2023-05-12 01:10:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}  /* bi warning vermesin diye unique bi attribüt verildi, başka bişi de olabilirdi */
+                item={item}
+            />
+        )
+    })        
+    
+    
+    
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
